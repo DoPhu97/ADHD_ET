205d394ef2bf4fee698de0383fead940c5ba4aad
DoPhu97 <157604643+DoPhu97@users.noreply.github.com>
2025-03-07 07:15:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ struct DataRow {
 
 void readFile(const std::string &filename, std::vector<DataRow> &data) {
 
-	std::ifstream file("data.txt"); // Open the file
+	std::ifstream file(filename); // Open the file
 	if (!file.is_open()) {
 		std::cerr << "Error: Unable to open file!" << std::endl;
 	}
@@ -42,7 +42,7 @@ void readFile(const std::string &filename, std::vector<DataRow> &data) {
 	 google::InitGoogleLogging(argv[0]);
 	 
 	 std::vector<DataRow> data;
-	 readFile("gaze_data.txt", data);
+	 readFile("data.txt", data);
 	 VecPair3 lights;//cm
 	 lights.left = Vec3(-121.038529804592, 70.3381501484736, 18.0648253141379);
 	 lights.right = Vec3(181.874354550426, -23.7875639906384, -3.03395914937812);
@@ -55,7 +55,7 @@ void readFile(const std::string &filename, std::vector<DataRow> &data) {
 	camera.pixel_size_x = 0.0055;//mm
 	camera.pixel_size_y = 0.0055;
 	camera.effective_focal_length = sqrt(3080.82181401356*3080.82181401356 +
-		3043.17471172823*3043.17471172823)*camera.pixel_size_x/10;
+		3043.17471172823*3043.17471172823);
 	
 	//Scene parameters to get poi in pixels
 	//const double display_surface_size_x = ;
@@ -66,6 +66,11 @@ void readFile(const std::string &filename, std::vector<DataRow> &data) {
 	const double screen_pixel_size_y = 1200;
 	const Vec3 screen_center = Vec3(0.359697691901012, -182.304785904820, -124.866720047268);
 
+
+	Mat3x3 rotation_matrix;
+	rotation_matrix << 0.999055371785405, -0.00765182687905128, -0.0427763211618310,
+		-0.00765182687905128, 0.938017461597792, -0.346503522757054,
+		0.0427763211618310, 0.346503522757054, 0.937072833383196;
 	
 	std::vector<Vec3> targets;
 	std::vector<PupilGlint2> calibData;
@@ -86,20 +91,15 @@ void readFile(const std::string &filename, std::vector<DataRow> &data) {
 		std::cout << targets[i][0]<<" "<< targets[i][1]<<" "<< targets[i][2] << "\n";
 	}
 	*/
-	std::vector<std::vector<double>> results = 
-		combined_calibrate(calibData, targets, camera, lights);
+	std::vector<std::vector<double>> results = calibrate(
+		calibData, targets, camera, lights,	screen_center, rotation_matrix);
 	
 	
-	// Xuất kết quả
 	std::cout << "Calibration results:\n";
-	std::cout << "kq_left: " << results[0][0] << "\n";
-	std::cout << "kq_right: " << results[1][0] << "\n";
-	std::cout << "alpha: " << rad_to_deg(results[2][0]) << "\n";
-	std::cout << "beta: " << rad_to_deg(results[3][0]) << "\n";
-	std::cout << "R: " << results[4][0] << "\n";
-	std::cout << "K: " << results[5][0] << "\n";
-	
-	
+	std::cout << "alpha: " << rad_to_deg(results[0][0]) << "\n";
+	std::cout << "beta: " << rad_to_deg(results[1][0]) << "\n";
+	std::cout << "R: " << results[2][0] << "\n";
+	std::cout << "K: " << results[3][0] << "\n";
 	
 	return 0;
 }
